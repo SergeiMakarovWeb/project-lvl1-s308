0489cbf08fddd461b247c8ed3d38ae3902519b57
Sergei Makarov <sergeimakarovweb@gmail.com>
2018-07-12 13:17:27 +0000

    step 3 changes

--- src/index.js
+++ src/index.js
@@ -2,23 +2,21 @@ import readlineSync from 'readline-sync';
 
 const userName = readlineSync.question('May I have your name?');
 
-const greeting = () => {
-    return console.log(`Hello, ${userName}!`);
-};
+const greeting = () => console.log(`Hello, ${userName}!`);
 
 const evenGame = () => {
-    for (let i = 0; i < 3; i += 1) {
-        const number = Math.floor(Math.random() * 101);
-        const isNumEven = number % 2 === 0 ? 'yes' : 'no';
-        console.log(`Question: ${number}`);
-        const userAnswer = readlineSync.question('Your answer? ');
-        if (userAnswer !== isNumEven) {
-            console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${isNumEven}. Let's try again, ${userName}`);
-            return;
-        }
-        console.log('Correct!');
+  for (let i = 0; i < 3; i += 1) {
+    const number = Math.floor(Math.random() * 101);
+    const isNumEven = number % 2 === 0 ? 'yes' : 'no';
+    console.log(`Question: ${number}`);
+    const userAnswer = readlineSync.question('Your answer? ');
+    if (userAnswer !== isNumEven) {
+      console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${isNumEven}. Let's try again, ${userName}`);
+      return;
     }
-    return console.log(`Congratulations, ${userName}!`);
+    console.log('Correct!');
+  }
+  console.log(`Congratulations, ${userName}!`);
 };
 
 export { greeting, evenGame };
